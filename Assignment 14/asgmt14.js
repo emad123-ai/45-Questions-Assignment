cbfb5e99cd432ed4f3949cda63dd7b662556f7ab
Mohammad Emad Akhtar <143903661+emad123-ai@users.noreply.github.com>
2024-06-17 17:01:39 +0500

    Add files via upload

---
+++ Assignment 14/asgmt14.js
@@ -0,0 +1,4 @@
+var invitation = ["Max", "James", "Sarah"];
+console.log("You along with your family are invited to the annual dinner at my residence " + invitation[0]);
+console.log("You along with your family are invited to the annual dinner at my residence " + invitation[1]);
+console.log("You along with your family are invited to the annual dinner at my residence " + invitation[2]);
---
+++ Assignment 14/asgmt14.ts
@@ -0,0 +1,5 @@
+let invitation = ["Max" , "James" , "Sarah"]
+
+console.log("You along with your family are invited to the annual dinner at my residence " + invitation[0]);
+console.log("You along with your family are invited to the annual dinner at my residence " + invitation[1]);
+console.log("You along with your family are invited to the annual dinner at my residence " + invitation[2]);
