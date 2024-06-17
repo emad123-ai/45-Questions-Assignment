22e1b006659afc5022b804c79bfee0504c3528aa
Mohammad Emad Akhtar <143903661+emad123-ai@users.noreply.github.com>
2024-06-17 16:36:36 +0500

    Add files via upload

---
+++ Assignment 5/asgmt5.js
@@ -0,0 +1,2 @@
+var Famous_person = 'Imran Khan once said, "Absolutely Not"';
+console.log(Famous_person);
---
+++ Assignment 5/asgmt5.ts
@@ -0,0 +1,3 @@
+let Famous_person : string = 'Imran Khan once said, "Absolutely Not"'
+
+console.log(Famous_person)
