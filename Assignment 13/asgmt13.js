3bab576faa0c1d556016d4c7d5f93f7c8c368817
Mohammad Emad Akhtar <143903661+emad123-ai@users.noreply.github.com>
2024-06-17 16:59:41 +0500

    Add files via upload

---
+++ Assignment 13/asgmt13.js
@@ -0,0 +1,6 @@
+var myCar = ["Honda Civic", "BMW", , "Mercedes", "Lexus", "Audi"];
+console.log("I have always fall in love with the sporty look of " + myCar[0]);
+console.log("The classy look of " + myCar[1]);
+console.log("The most lavish car in terms of features is " + myCar[2]);
+console.log("The most luxurious SUV till date is " + myCar[3]);
+console.log("Stylish light of " + myCar[4]);
