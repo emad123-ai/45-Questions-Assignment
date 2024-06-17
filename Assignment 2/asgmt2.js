d03e281f041f9aaa93fd516cff36c823c1ae0cf6
Mohammad Emad Akhtar <143903661+emad123-ai@users.noreply.github.com>
2024-06-17 16:29:07 +0500

    Add files via upload

---
+++ Assignment 2/asgmt2.js
@@ -0,0 +1,2 @@
+var personName = "Mohammad Emad Akhtar";
+console.log('Hello', personName, "would you like to join us?");
---
+++ Assignment 2/asgmt2.ts
@@ -0,0 +1,4 @@
+let personName = "Mohammad Emad Akhtar";
+
+console.log('Hello' ,personName, "would you like to join us?");
+    
