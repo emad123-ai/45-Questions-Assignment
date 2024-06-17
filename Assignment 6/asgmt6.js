7866141e38cfc418c95c0ae967203393923e69fa
Mohammad Emad Akhtar <143903661+emad123-ai@users.noreply.github.com>
2024-06-17 16:38:19 +0500

    Add files via upload

---
+++ Assignment 6/asgmt6.js
@@ -0,0 +1,4 @@
+var whiteSpace = "\n\t Mohammad Emad Akhtar \t\n";
+console.log(whiteSpace);
+var WithoutwhiteSpace = whiteSpace.trim();
+console.log(WithoutwhiteSpace);
---
+++ Assignment 6/asgmt6.ts
@@ -0,0 +1,6 @@
+let whiteSpace = "\n\t Mohammad Emad Akhtar \t\n";
+
+console.log(whiteSpace);
+
+let WithoutwhiteSpace = whiteSpace.trim();
+console.log(WithoutwhiteSpace);
