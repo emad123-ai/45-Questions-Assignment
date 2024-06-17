2b054e176612cadc87f365809ca8cfd5bf9af868
Mohammad Emad Akhtar <143903661+emad123-ai@users.noreply.github.com>
2024-06-17 16:50:29 +0500

    Add files via upload

---
+++ Assignment 10/asgmt10.js
@@ -0,0 +1,9 @@
+var myName = "Mohammad Emad Akhtar";
+console.log(myName);
+var toDate = "21/04/2024";
+console.log(toDate);
+var myComment = "this program is designed to reveal my favrotie number";
+console.log(myComment);
+var myFavoritenum = 10;
+var a = "my favrotie number is";
+console.log("my favorite number is", myFavoritenum);
---
+++ Assignment 10/asgmt10.ts
@@ -0,0 +1,16 @@
+let myName : string = "Mohammad Emad Akhtar";
+
+console.log(myName);
+
+let toDate : any = "21/04/2024";
+
+console.log(toDate);
+
+let myComment : string = "this program is designed to reveal my favrotie number";
+console.log(myComment);
+
+let myFavoritenum : any = 10;
+
+let a = "my favrotie number is"
+
+console.log("my favorite number is" , myFavoritenum)
