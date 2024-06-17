87e9c7f4ddea8735ea86b48c7eaa8ec4890b696b
Mohammad Emad Akhtar <143903661+emad123-ai@users.noreply.github.com>
2024-06-17 17:05:18 +0500

    Add files via upload

---
+++ Assignment 16/asgmt16.ts
@@ -0,0 +1,9 @@
+let invitation = ["Ben" , "Max" , "James" , "Sarah" , "Mark"]
+
+console.log("You along with your family are invited to the annual dinner at my residence " + invitation[1]);
+console.log("You along with your family are invited to the annual dinner at my residence " + invitation[2]);
+console.log("Unfortunately Sarah will be able to make it to the annual dinner");
+console.log("You along with your family are invited to the annual dinner at my residence " + invitation[4]);
+console.log("You along with your family are invited to the annual dinner at my residence " + invitation[0]);
+console.log("Hurray! I'd love to inform you guys that we have found a bigger table");
+
