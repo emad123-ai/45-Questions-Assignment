064816d1388ffc2f191c3fc4c7ef6dfbc165c6a5
Mohammad Emad Akhtar <143903661+emad123-ai@users.noreply.github.com>
2024-06-17 16:46:57 +0500

    Add files via upload

---
+++ Assignment 7/asgmt7.js
@@ -0,0 +1,12 @@
+var num1 = 4;
+var num2 = 4;
+console.log(num1 + num2);
+var num3 = 10;
+var num4 = 2;
+console.log(num3 - num4);
+var num5 = 2;
+var num6 = 4;
+console.log(num5 * num6);
+var num7 = 16;
+var num8 = 2;
+console.log(num7 / num8);
