aeefa1e9368c4a1a2c19dca0a5ed63b59a814e03
Mohammad Emad Akhtar <143903661+emad123-ai@users.noreply.github.com>
2024-06-17 17:07:11 +0500

    Add files via upload

---
+++ Assignment 17/asgmt17.ts
@@ -0,0 +1,7 @@
+    let invitation = ["Ben" , "Max" , "James" , "Sarah" , "Mark"]
+
+console.log("Unfortunately Sarah will be able to make it to the annual dinner");
+console.log("You along with your family are invited to the annual dinner at my residence " + invitation[4]);
+console.log("You along with your family are invited to the annual dinner at my residence " + invitation[0]);
+
+
