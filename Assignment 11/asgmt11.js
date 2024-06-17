d1bc4c470a28a1fc2d70032648d18f9271915b86
Mohammad Emad Akhtar <143903661+emad123-ai@users.noreply.github.com>
2024-06-17 16:54:42 +0500

    Add files via upload

---
+++ Assignment 11/asgmt11.js
@@ -0,0 +1,4 @@
+var Names = ["Ebad", "Farhan", "Talha"];
+console.log(Names[0]);
+console.log(Names[1]);
+console.log(Names[2]);
---
+++ Assignment 11/asgmt11.ts
@@ -0,0 +1,5 @@
+let Names = ["Ebad" , "Farhan" , "Talha"];
+
+console.log(Names[0]);
+console.log(Names[1]);
+console.log(Names[2]);
