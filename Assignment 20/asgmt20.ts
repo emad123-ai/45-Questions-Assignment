4ecfb2f0bd0ee88916769650d0f19fbcf07d83a4
Mohammad Emad Akhtar <143903661+emad123-ai@users.noreply.github.com>
2024-06-17 17:26:40 +0500

    Add files via upload

---
+++ Assignment 20/asgmt20.ts
@@ -0,0 +1,9 @@
+let Rivers = [ "Karachi" , "Lahore" , "Islamabad" , "Peshawar" ]; 
+
+console.log(Rivers)
+
+console.log("Welcome to " + Rivers[0]);
+console.log("Welcome to " + Rivers[1]);
+console.log("Welcome to " + Rivers[2]);
+console.log("Welcome to " + Rivers[3]);
+
