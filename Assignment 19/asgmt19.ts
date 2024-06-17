6bf82f846fa9a601581ba435de95d87a93d6d617
Mohammad Emad Akhtar <143903661+emad123-ai@users.noreply.github.com>
2024-06-17 17:20:50 +0500

    Add files via upload

---
+++ Assignment 19/asgmt19.ts
@@ -0,0 +1,4 @@
+let Invitation = ["Ben" , "Mark"];
+console.log("You along with your family are invited at the annual dinner " + Invitation[0]);
+console.log("You along with your family are invited at the annual dinner " + Invitation[1]);
+console.log("We have invited 2 guests at the annual dinner");
