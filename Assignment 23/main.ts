45798de87e5c5a820eb1d83119ec23c98464894b
Mohammad Emad Akhtar <143903661+emad123-ai@users.noreply.github.com>
2024-06-18 14:52:34 +0500

    Add files via upload

---
+++ Assignment 23/main.ts
@@ -0,0 +1,27 @@
+let fruit = "apple";
+
+console.log("Is the fruit == apple?. I predict it is True");
+console.log(fruit == "apple");
+
+fruit = "banana";
+
+console.log("\nIs the fruit == apple?. I predict it is False");
+console.log(fruit == "apple");
+
+console.log("\nIs the fruit != apple?. I predict it is True");
+console.log(fruit != "apple");
+
+let a = 2;
+
+console.log("\nIs a < 3. I predict it is True");
+console.log(a < 3);
+
+console.log("\nIs a > 3. I predict it is False");
+console.log(a > 3);
+
+console.log("\nIs a <= 3. I predict it is True");
+console.log(a <= 3);
+
+console.log("\nIs a >= 3. I predict it is False");
+console.log(a >= 3);
+
